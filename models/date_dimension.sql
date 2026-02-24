8b275d7e4896ae4a38b4b927a886a7e8afad9974
Namara Nadaf <nam.nadaf92@gmail.com>
2026-02-24 16:32:36 +0000

    date_macro

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -3,16 +3,8 @@ select
 to_timestamp(started_at) as started_at,
 DATE(to_timestamp(started_at) ) AS DATE_STARTED_AT,
 HOUR(to_timestamp(started_at) ) AS HOUR_STARTED_AT,
-CASE WHEN 
-DAYNAME(to_timestamp(started_at) ) IN ('Sat', 'Sun')
-then 'WEEKEND'
-else 'Businessday'
-end AS DAY_TYPE,
-CASE
-WHEN MONTH(to_timestamp(started_at) ) IN (12,1,2) then 'Winter'
-WHEN MONTH(to_timestamp(started_at) ) IN (3,4,5) then 'Summer'
-WHEN MONTH(to_timestamp(started_at) ) IN (6,7,8) then 'Monsoon'
-else 'Autumn' end as Season_of_year
+{{day_type('started_at')}} AS DAY_TYPE,
+{{ get_season('started_at') }} as season_of_year
 from {{ source('demo', 'bike') }}
 
 )
